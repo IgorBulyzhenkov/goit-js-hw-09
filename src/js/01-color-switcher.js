89484b409cb9679cf833dce68489332c119d6404
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-06 17:26:58 +0300

    src

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+// console.log(body);
+let timerId = null;
+
+body.addEventListener('click', onStartBtn);
+body.addEventListener('click', onStopBtn);
+
+function onStopBtn(e) {
+  if (e.target.dataset === stopBtn.dataset) {
+      startBtn.disabled = false;
+      clearInterval(timerId);
+  }
+}
+
+function onStartBtn(e) {
+  if (e.target.dataset === startBtn.dataset) {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    body.style.backgroundColor = getRandomHexColor();
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
